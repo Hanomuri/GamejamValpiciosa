f1eed6b2ba875992c5eb76ef665b119a07632ffe
Kartofellord <oscar.felipepf@gmail.com>
2024-06-15 17:25:00 -0400

    holacarlita

---
+++ include/menu.h
@@ -0,0 +1,25 @@
+#include "raylib.h"
+
+class menu {
+private:
+    int point = 0;
+    int menuButton[4];
+public:
+    menu(/* args */);
+    ~menu();
+    void menuMovement();
+};
+
+void menu::menuMovement() {
+    if(IsKeyPressed)
+}
+
+menu::menu(/* args */)
+{
+}
+
+menu::~menu()
+{
+}
+
+
