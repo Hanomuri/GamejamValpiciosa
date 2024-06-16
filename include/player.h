e34da6597d0f4b96b0d8a1357e137747db9bce3b
tapsujan <simonsobenes@gmail.com>
2024-06-16 12:47:14 -0400

    music

--- include/player.h
+++ include/player.h
@@ -8,7 +8,7 @@ typedef struct Player : Entity {
   using Entity::Move, Entity::ToggleCollider;
   Camera2D m_camera = {0, 0, 0, 0, 0, 0};
   Todo todo = {};
-
+  int dangerLevel = 0;
   Player();
   void SetCamera();
   void Update();
